8b66148b5219582feca2ac8903907dc1d2e5f0d3
AngelCorral0 <angel.corral.olivares@gmail.com>
2024-06-10 12:49:33 +0200

    creacion de tasks-wrapper y espaciado de buttons

--- src/components/Lists.tsx
+++ src/components/Lists.tsx
@@ -1,76 +1,75 @@
-
 interface ItemProps {
-    title: string;
-    description?: string;
+  title: string;
+  description?: string;
 }
 
 interface ToDoProps {
-    title : string,
+  title: string;
 }
 
 export function ToDoWrapper({ title }: ToDoProps) {
-
-
-    return (
-        <main className="to-do-wrapper">
-            <div className="hero-title">
-                <h1>
-                    { title }
-                </h1>
-            </div>
-            <div className="buttons-wrapper">
-                <AddButton />
-                <FilterButton />
-            </div>
-            <List />
-        </main>
-    )
+  return (
+    <main className="to-do-wrapper">
+      <div className="hero-title">
+        <h1>{title}</h1>
+      </div>
+      <div className="task-wrapper">
+        <div className="buttons-wrapper">
+          <AddButton />
+          <FilterButton />
+        </div>
+        <List />
+      </div>
+    </main>
+  );
 }
 
 export function AddButton() {
-
-    return (
-        <button>
-            Add +
-        </button>
-    )
+  return <button>Add +</button>;
 }
 
 export function FilterButton() {
-
-    return (
-        <>
-            <select name="options" id="options">
-                <option value="all">All</option>
-                <option value="doing">Doing</option>
-                <option value="done">Done</option>
-            </select>
-        </>
-    )
+  return (
+    <>
+      <select name="options" id="options">
+        <option value="all">All</option>
+        <option value="doing">Doing</option>
+        <option value="done">Done</option>
+      </select>
+    </>
+  );
 }
 
 export function ItemList({ title, description }: ItemProps) {
-    return (
-        <div className="list-item">
-            <h1>{title}</h1>
-            {description && <p>{description}</p>}
-        </div>
-    );
+  return (
+    <div className="list-item">
+      <h1>{title}</h1>
+      {description && <p>{description}</p>}
+    </div>
+  );
 }
 
 export function List() {
-    let data = [
-        {
-            title: "hello",
-            description: "This is a description"
-        },
-    ];
+  let data = [
+    {
+      title: "hello",
+      description: "This is a description",
+    },
+    {
+      title: "hello",
+      description: "This is a description",
+    },
+  ];
 
-    return (
-        <div className="list-wrapper">
-            {data.map((item, index) => (
-                <ItemList key={index} title={item.title} description={item.description} />
-            ))}
-        </div>
-    );
+  return (
+    <div className="list-wrapper">
+      {data.map((item, index) => (
+        <ItemList
+          key={index}
+          title={item.title}
+          description={item.description}
+        />
+      ))}
+    </div>
+  );
 }
