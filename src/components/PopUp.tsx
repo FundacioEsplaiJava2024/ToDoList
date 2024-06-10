b871d34e8989f40e83baa8ad1f91f00ddb6ca3df
Daniela-Rf5 <danielarodriguezlainez@gmail.com>
2024-06-10 13:58:27 +0200

    Component popUp logica

---
+++ src/components/PopUp.tsx
@@ -0,0 +1,35 @@
+export function PopUp(){
+    return (
+        <div className="pop-up-wrapper"></div>
+    )
+}
+
+export function PopUpAdd(){
+    return (
+        <>
+        <h1>Prueba</h1>
+        <span>10/06/2024</span>
+        <span>Deadline</span>
+        <span>Urgente</span>
+        <span>To Do</span>
+        <p>Descripción</p>
+        <button>Add</button>
+        </>
+    )
+}
+
+
+export function PopUpView(){
+    return (
+        <>
+        <h1>Prueba</h1>
+        <span>10/06/2024</span>
+        <span>Deadline</span>
+        <span>Urgente</span>
+        <span>To Do</span>
+        <p>Descripción</p>
+        <button>Update</button>
+        <button>Delete</button>
+        </>
+    )
+}
