4f26bccbffeac941248bd361f700cff548f4d2e3
rvst312 <115166784+rvst312@users.noreply.github.com>
2024-06-10 09:17:20 +0200

    margin 0 padding 0

--- src/components/Lists.tsx
+++ src/components/Lists.tsx
@@ -1,9 +1,31 @@
 
-export function List() {
-
-    return(
-        <h1>Hello</h1>
-    )
+interface ItemProps {
+    title: string;
+    description?: string;
 }
 
+export function ItemList({ title, description }: ItemProps) {
+    return (
+        <div>
+            <h1>{title}</h1>
+            {description && <p>{description}</p>}
+        </div>
+    );
+}
 
+export function List() {
+    const data = [
+        {
+            title: "hello",
+            description: "This is a description"
+        },
+    ];
+
+    return (
+        <>
+            {data.map((item, index) => (
+                <ItemList key={index} title={item.title} description={item.description} />
+            ))}
+        </>
+    );
+}
